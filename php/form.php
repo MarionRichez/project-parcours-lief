9f65a285943f61cc9eef30b231c94ddb46efcd11
Richez Marion <richez.m95@gmail.com>
2020-05-01 16:52:51 +0200

    création du php lors d'envoi de mail

---
+++ php/form.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Location: https://parcours-lief.be');
+
+$mailVIP = "";
+$mailOVH = "";
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$subject = "TFE" . ' ' . $_POST['subject'];
+$msg = $_POST['message'];
+
+ini_set("SMTP", '...');
+
+$headers = array (
+    "MINE-Version" => 1.0,
+    "Content-type" => "text/html; charset=iso-8859-1",
+    "To" => $mailOVH,
+    "From" => $mail,
+    "Reply-To" => $mail,
+    "Disposition-Notification-To" => $mailOVH,
+    "X-Priority" => 1,
+    "X-MSMail-Priority" => "High"
+);
+
+$CR_Mail = true;
+$CR_Mail = @mail($mailVIP, $subject, $mail, $headers);
+if ($CR_Mail === false) {
+    echo "### CR_Mail=$CR_Mail - error sending email <br> \n";
+} else {
+    echo " *** CR_Mail=$CR_Mail - mail sent <br> \n";
+}
