3c951b54ece4c0825c39c72f331adca770998fc4
Richez Marion <richez.m95@gmail.com>
2020-05-05 16:25:24 +0200

    modification du php

--- php/form.php
+++ php/form.php
@@ -2,11 +2,10 @@
 
 header('Location: https://parcours-lief.be');
 
-$mailVIP = "richez.m95@gmail.com";
-$mailOVH = "richez.m@marion-richez.be";
+$mailVIP = "parcours.lief@gmail.com";
 $name = $_POST['name'];
 $mail = $_POST['email'];
-$subject = "TFE" . ' ' . $_POST['subject'];
+$subject = "TFE" . ' - ' . $_POST['subject'];
 $msg = $_POST['message'];
 
 ini_set("SMTP", 'smtp.marion-richez.be');
@@ -14,10 +13,10 @@ ini_set("SMTP", 'smtp.marion-richez.be');
 $headers = array (
     "MINE-Version" => 1.0,
     "Content-type" => "text/html; charset=iso-8859-1",
-    "To" => $mailOVH,
+    "To" => $mailVIP,
     "From" => $mail,
     "Reply-To" => $mail,
-    "Disposition-Notification-To" => $mailOVH,
+    "Disposition-Notification-To" => $mailVIP,
     "X-Priority" => 1,
     "X-MSMail-Priority" => "High"
 );
